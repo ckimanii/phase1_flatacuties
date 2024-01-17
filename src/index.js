d07c28e31a7363daab88372852f7afdd3e4c1a30
Kimani <alex.kimani@student.moringaschool.com>
2024-01-17 09:43:42 +0300

    Done with assignment

---
+++ src/index.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+  const characterBar = document.getElementById('character-bar');
+  const characterImage = document.getElementById('image');
+  const characterName = document.getElementById('name');
+  const votesForm = document.getElementById('votes-form');
+  const votesInput = document.getElementById('votes');
+  const flatacutiesApi = "https://api.npoint.io/2d7c79c29f56ab23c201/characters/"
+  
+  fetch(`${flatacutiesApi}`)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(character => {
+        const characterNameElement = document.createElement('p');
+        characterNameElement.textContent = character.name;
+        characterNameElement.addEventListener('click', () => {
+          characterImage.src = character.image;
+          characterImage.alt = character.name;
+          characterName.textContent = character.name;
+        });
+        characterBar.appendChild(characterNameElement);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+  votesForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    const characterVotes = document.createElement('p');
+    const characterVotesValue = parseInt(votesInput.value);
+    const characterVoteCount = characterVotesValue || 0;
+
+    if (characterVotesValue) {
+      characterVotes.textContent = `Total Votes: ${characterVotesValue}`;
+      characterBar.appendChild(characterVotes);
+    }
+
+    votesInput.value = '';
+  });
+};
+
+
